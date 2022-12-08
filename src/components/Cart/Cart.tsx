cb007baf7ae352bcac9eed4f2dae774c3b466c75
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2022-12-08 15:52:22 +0800

    addingProductasProp

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,10 +1,32 @@
 import { ProductsWrapper, Title } from './Cart.styled';
+import { FC, useEffect, useState } from 'react';
+import { Product } from '../../models';
+import { ProductCard } from '../ProductCard';
 
-export const Cart = () => {
+interface productProps{
+  products : Product[];
+}
+
+export const Cart : FC<productProps> = (props) : JSX.Element => {
+
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    let total = 0;
+    props.products.forEach((product) => {
+      total += product.price;
+    });
+    setTotalPrice(total);
+  }, [props.products]);
+
   return (
     <>
-      <Title>Your cart total is 00.00$</Title>
-      <ProductsWrapper></ProductsWrapper>
+      <Title>Your cart total is ${totalPrice}.00</Title>
+      <ProductsWrapper>
+        {props.products.map((data, index) => 
+          <ProductCard key={index} {...data} />
+        )}
+      </ProductsWrapper>
     </>
   );
 };
